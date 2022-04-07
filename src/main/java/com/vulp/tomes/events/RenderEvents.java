e03f1b8360c969e9d5b74ee83aea9a216dcb64f1
Vulpie <sammahergaming@gmail.com>
2022-04-07 16:49:05 +1000

    Attempted Apotheosis compat fixes (didn't work)

--- src/main/java/com/vulp/tomes/events/RenderEvents.java
+++ src/main/java/com/vulp/tomes/events/RenderEvents.java
@@ -1,17 +1,41 @@
 package com.vulp.tomes.events;
 
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
 import com.vulp.tomes.Tomes;
+import com.vulp.tomes.config.TomesConfig;
+import com.vulp.tomes.enchantments.EnchantClueHolder;
+import com.vulp.tomes.init.EnchantmentInit;
 import com.vulp.tomes.init.ItemInit;
 import com.vulp.tomes.items.TomeItem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.EnchantmentContainer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.*;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderHandEvent;
+import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid= Tomes.MODID, bus=Mod.EventBusSubscriber.Bus.FORGE, value=Dist.CLIENT)
 public class RenderEvents {
+
+    private static boolean HOOK_TOGGLE = false;
 
     @SubscribeEvent
     public static void onRenderHandEvent(RenderHandEvent event) {
@@ -30,6 +54,96 @@ public class RenderEvents {
             minecraft.getFirstPersonRenderer().renderItemInFirstPerson(minecraft.player, event.getPartialTicks(), event.getInterpolatedPitch(), event.getHand(), event.getSwingProgress(), newStack, event.getEquipProgress(), event.getMatrixStack(), event.getBuffers(), event.getLight());
             event.setCanceled(true);
         }
+    }
+
+    @SubscribeEvent
+    public static void onRenderTooltipEvent(RenderTooltipEvent.Pre event) {
+        // Latch helps make sure that calling func_243308_b below doesn't cause an infinite loop of forge hook events.
+        if (HOOK_TOGGLE) {
+            HOOK_TOGGLE = false;
+            return;
+        }
+        Minecraft minecraft = Minecraft.getInstance();
+        ClientPlayerEntity player = minecraft.player;
+        if (player != null) {
+            PlayerInventory inventory = player.inventory;
+            boolean hasTome = inventory != null && Arrays.stream(new ItemStack[]{inventory.getCurrentItem(), inventory.offHandInventory.get(0)}).anyMatch(item -> item.getItem() instanceof TomeItem && EnchantmentHelper.getEnchantments(item).containsKey(EnchantmentInit.linguist));
+            if (TomesConfig.linguist_enabled.get() && hasTome) {
+                Screen screen = minecraft.currentScreen;
+                if (screen instanceof ContainerScreen && ((ContainerScreen<?>) screen).getContainer() instanceof EnchantmentContainer) {
+                    ContainerScreen<?> containerScreen = (ContainerScreen<?>) screen;
+                    EnchantmentContainer container = (EnchantmentContainer) containerScreen.getContainer();
+                    EnchantClueHolder clueHolder = EnchantClueHolder.decodeClues(container.enchantClue);
+                    for (int j = 0; j < 3; ++j) {
+                        List<Pair<Enchantment, Integer>> clueList = clueHolder.getData(j);
+                        List<ITextComponent> list = Lists.newArrayList();
+                        int k = 0;
+                        int i1 = j + 1;
+                        ItemStack stack = container.tableInventory.getStackInSlot(0);
+                        if (isPointInRegion(60, 14 + 19 * j, 108, 17, event.getX(), event.getY(), containerScreen.getGuiLeft(), containerScreen.getGuiTop()) && clueList.size() > 0 && !stack.isEmpty() && !stack.isEnchanted() && stack.isEnchantable()) {
+                            for (Pair<Enchantment, Integer> clue : clueList) {
+                                if (clue.getFirst() != null) {
+                                    list.add(getDisplayName(clue.getFirst(), clue.getSecond()));
+                                } else {
+                                    list.add(new StringTextComponent(". . .").mergeStyle(TextFormatting.AQUA));
+                                }
+                            }
+                            if (clueList.isEmpty()) {
+                                list.add(new StringTextComponent(""));
+                                list.add(new TranslationTextComponent("forge.container.enchant.limitedEnchantability").mergeStyle(TextFormatting.RED));
+                            } else if (!player.abilities.isCreativeMode) {
+                                list.add(StringTextComponent.EMPTY);
+                                if (player.experienceLevel < k) {
+                                    list.add((new TranslationTextComponent("container.enchant.level.requirement", (container).enchantLevels[j])).mergeStyle(TextFormatting.RED));
+                                } else {
+                                    IFormattableTextComponent iformattabletextcomponent;
+                                    if (i1 == 1) {
+                                        iformattabletextcomponent = new TranslationTextComponent("container.enchant.lapis.one");
+                                    } else {
+                                        iformattabletextcomponent = new TranslationTextComponent("container.enchant.lapis.many", i1);
+                                    }
+
+                                    list.add(iformattabletextcomponent.mergeStyle(container.getLapisAmount() >= i1 ? TextFormatting.GRAY : TextFormatting.RED));
+                                    IFormattableTextComponent iformattabletextcomponent1;
+                                    if (i1 == 1) {
+                                        iformattabletextcomponent1 = new TranslationTextComponent("container.enchant.level.one");
+                                    } else {
+                                        iformattabletextcomponent1 = new TranslationTextComponent("container.enchant.level.many", i1);
+                                    }
+
+                                    list.add(iformattabletextcomponent1.mergeStyle(TextFormatting.GRAY));
+                                }
+                            }
+                            HOOK_TOGGLE = true;
+                            containerScreen.func_243308_b(event.getMatrixStack(), list, event.getX(), event.getY());
+                            event.setCanceled(true);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static boolean isPointInRegion(int x, int y, int width, int height, double mouseX, double mouseY, int guiLeft, int guiTop) {
+        mouseX = mouseX - (double)guiLeft;
+        mouseY = mouseY - (double)guiTop;
+        return mouseX >= (double)(x - 1) && mouseX < (double)(x + width + 1) && mouseY >= (double)(y - 1) && mouseY < (double)(y + height + 1);
+    }
+
+    private static ITextComponent getDisplayName(Enchantment enchantment, int level) {
+        IFormattableTextComponent iformattabletextcomponent = new TranslationTextComponent(enchantment.getName());
+        if (enchantment.isCurse()) {
+            iformattabletextcomponent.mergeStyle(TextFormatting.RED);
+        } else {
+            iformattabletextcomponent.mergeStyle(TextFormatting.AQUA);
+        }
+
+        if (level != 1 || enchantment.getMaxLevel() != 1) {
+            iformattabletextcomponent.appendString(" ").appendSibling(new TranslationTextComponent("enchantment.level." + level));
+        }
+
+        return iformattabletextcomponent;
     }
 
 }
--- src/main/java/com/vulp/tomes/mixin/EnchantmentContainerMixin.java
+++ src/main/java/com/vulp/tomes/mixin/EnchantmentContainerMixin.java
@@ -1,82 +1,71 @@
 package com.vulp.tomes.mixin;
 
 import com.mojang.datafixers.util.Pair;
+import com.vulp.tomes.Tomes;
 import com.vulp.tomes.enchantments.EnchantClueHolder;
 import com.vulp.tomes.network.TomesPacketHandler;
 import com.vulp.tomes.network.messages.ServerEnchantmentClueMessage;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentData;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.container.EnchantmentContainer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.registry.Registry;
 import net.minecraftforge.fml.network.PacketDistributor;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 @Mixin(EnchantmentContainer.class)
-public abstract class EnchantmentContainerMixin extends Container {
-
-    @Shadow @Final public int[] enchantLevels;
-
-    @Shadow @Final public IInventory tableInventory;
-
-    @Shadow @Final public IWorldPosCallable worldPosCallable;
-
-    @Shadow public abstract List<EnchantmentData> getEnchantmentList(ItemStack stack, int enchantSlot, int level);
+public class EnchantmentContainerMixin {
 
     @Shadow public int[] enchantClue;
-
+    @Shadow @Final public Random rand;
     @Shadow @Final public int[] worldClue;
 
-    @Shadow @Final public Random rand;
+    private static EnchantClueHolder HOLDER = new EnchantClueHolder();
+    private static boolean ENCHANT_LATCH = true;
 
-    protected EnchantmentContainerMixin(@Nullable ContainerType<?> type, int id) {
-        super(type, id);
+    // NOTE: This method is not actually called in Apotheosis, it is a near identical method set up by Shadow instead. Ergo, this method is never called when Apotheosis is installed.
+    // To fix, maybe hope that random is consistent, and either test for his mod and then reflect the info you need to calculate the data for yourself, or find a way to alter his code. Communicate with him.
+    @Inject(method = "getEnchantmentList", at = @At("RETURN"))
+    public void getEnchantmentList(ItemStack stack, int enchantSlot, int level, CallbackInfoReturnable<List<EnchantmentData>> cir) {
+        if (ENCHANT_LATCH) {
+            List<EnchantmentData> list = cir.getReturnValue();
+            if (!list.isEmpty()) {
+                List<Pair<Enchantment, Integer>> cluedPairingList = new java.util.ArrayList<>(Collections.emptyList());
+                for (EnchantmentData enchantmentData : list) {
+                    cluedPairingList.add(new Pair<>(enchantmentData.enchantment, enchantmentData.enchantmentLevel));
+                }
+                HOLDER.setData(enchantSlot, cluedPairingList);
+                EnchantmentData data = list.get(this.rand.nextInt(list.size()));
+                this.enchantClue[enchantSlot] = Registry.ENCHANTMENT.getId(data.enchantment);
+                this.worldClue[enchantSlot] = data.enchantmentLevel;
+            }
+            if (enchantSlot == 2) {
+                this.enchantClue = EnchantClueHolder.encodeClues(HOLDER, this.enchantClue);
+                TomesPacketHandler.instance.send(PacketDistributor.ALL.noArg(), new ServerEnchantmentClueMessage(getThis().windowId, this.enchantClue));
+                HOLDER = new EnchantClueHolder();
+            }
+        } else {
+            ENCHANT_LATCH = true;
+        }
     }
 
-    @Inject(at = @At("TAIL"), method = "onCraftMatrixChanged", cancellable = true)
-    public void onCraftMatrixChanged(IInventory inventoryIn, CallbackInfo ci) {
-        if (inventoryIn == this.tableInventory) {
-            ItemStack itemstack = inventoryIn.getStackInSlot(0);
-            if (!itemstack.isEmpty() && itemstack.isEnchantable()) {
-                this.worldPosCallable.consume((p_217002_2_, p_217002_3_) -> {
-                    EnchantClueHolder holder = new EnchantClueHolder();
-                    for(int j1 = 0; j1 < 3; ++j1) {
-                        if (this.enchantLevels[j1] > 0) {
-                            List<EnchantmentData> list = this.getEnchantmentList(this.tableInventory.getStackInSlot(0), j1, this.enchantLevels[j1]);
-                            if (!list.isEmpty()) {
-                                List<Pair<Enchantment, Integer>> cluedPairingList = new java.util.ArrayList<>(Collections.emptyList());
+    @Inject(at = @At(value = "TAIL", target = "Lnet/minecraft/inventory/container/EnchantmentContainer;getEnchantmentList(Lnet/minecraft/item/ItemStack;II)Ljava/util/List;"), method = "enchantItem")
+    public void enchantItem(PlayerEntity playerIn, int id, CallbackInfoReturnable<Boolean> cir) {
+        ENCHANT_LATCH = false;
+    }
 
-                                for (EnchantmentData enchantmentData : list) {
-                                    cluedPairingList.add(new Pair<>(enchantmentData.enchantment, enchantmentData.enchantmentLevel));
-                                }
-
-                                holder.setData(j1, cluedPairingList);
-
-                                EnchantmentData enchantmentdata = list.get(this.rand.nextInt(list.size()));
-                                this.enchantClue[j1] = Registry.ENCHANTMENT.getId(enchantmentdata.enchantment);
-                                this.worldClue[j1] = enchantmentdata.enchantmentLevel;
-                            }
-                        }
-                    }
-                    this.enchantClue = EnchantClueHolder.encodeClues(holder, this.enchantClue);
-                    TomesPacketHandler.instance.send(PacketDistributor.ALL.noArg(), new ServerEnchantmentClueMessage(this.windowId, this.enchantClue));
-                });
-            }
-        }
+    // Allows us to reference the current instance of the class the mixin is injecting.
+    private EnchantmentContainer getThis() {
+        return ((EnchantmentContainer)(Object)this);
     }
 
 }
